7e133edfff399dfbf40ba2b2f6ccb18ba40fe8c5
wilfriedmuffins <wilfried.moffen@live.fr>
2020-03-04 01:06:24 +0100

    modification de l'utilisation du parametre nbre

--- escalier.js
+++ escalier.js
@@ -1,6 +1,6 @@
 
-function escalier(n) {
-  var n = parseInt(process.argv.slice(2)); // convert String to int
+function escalier(nbre) {
+  var n = parseInt(nbre); // convert String to int
   var count = n;
   for(var i=1 ; i<n+1; i++){
     console.log(" ".repeat(count) + "#".repeat(i));
