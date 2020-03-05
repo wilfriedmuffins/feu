49abc1fb93411f360763c9f70e8d5d6defe81c30
wilfriedmuffins <wilfried.moffen@live.fr>
2020-03-06 00:28:38 +0100

    dif en carac espace

--- maj.js
+++ maj.js
@@ -1,31 +1,38 @@
 const [nd, sc, c1] = process.argv;
-
-
 function maj(a){
-const longer = a.length;
-//let phrase = [];
-/*console.log(a.slice(0, 1));
-console.log(a.slice(1, 2));
-console.log(a.slice(2, 3));
-console.log(a.slice(3, 4));
-console.log(a.slice(, 2));*/
+  const longer = a.length;
+  let letter;
+  let rletter;
+  const index = /e/i; ;
+  let esp = [];
+  //console.log(a.replace(/e/y, 'y'));
+  /*for(i=0 ; i<2 ; i++){
+    console.log(a.replace(index, 'L'));
+    //console.log(a);
+    console.log(a.replace(index, 'M'));
+    //console.log(a);
+  }*/
 
 
-
- console.log(longer);
- for (i=0 ; i<longer ; i++){
-   if (a.slice(i, i+1) == " "){
-     console.log('espace');
-   }else {
-
-     console.log('caractére');
-     console.log(a.slice(i, i+1).toUpperCase());
+   //console.log(longer);
+   for (i=0 ; i<longer ; i++){
+     if (a.slice(i, i+1) == " "){
+       console.log('espace');
+       esp.push(i);
+     }else {
+       //prendre un lettre
+      //console.log(a.slice(i, i+1));
+       letter = a.slice(i, i+1);
+       console.log(letter);
+       //mettre en maj
+       rletter = letter.toUpperCase();
+       //remplacer + affiché
+    //   index= /rletter/y;
+       console.log(a.replace(index, rletter));
+     }
    }
- }
-
-console.log(a);
-
+   console.log(esp);
 }
 
+
 maj(c1);
-//process.argv.slice(2)
