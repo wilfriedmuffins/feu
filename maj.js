f41548637ed0f326bbfdc2c14c613c965f2834ec
wilfriedmuffins <wilfried.moffen@live.fr>
2020-03-06 11:08:48 +0100

    done

--- maj.js
+++ maj.js
@@ -1,38 +1,33 @@
 const [nd, sc, c1] = process.argv;
 function maj(a){
   const longer = a.length;
-  let letter;
-  let rletter;
-  const index = /e/i; ;
-  let esp = [];
-  //console.log(a.replace(/e/y, 'y'));
-  /*for(i=0 ; i<2 ; i++){
-    console.log(a.replace(index, 'L'));
-    //console.log(a);
-    console.log(a.replace(index, 'M'));
-    //console.log(a);
-  }*/
-
-
-   //console.log(longer);
-   for (i=0 ; i<longer ; i++){
-     if (a.slice(i, i+1) == " "){
-       console.log('espace');
-       esp.push(i);
-     }else {
-       //prendre un lettre
-      //console.log(a.slice(i, i+1));
-       letter = a.slice(i, i+1);
-       console.log(letter);
-       //mettre en maj
-       rletter = letter.toUpperCase();
-       //remplacer + affiché
-    //   index= /rletter/y;
-       console.log(a.replace(index, rletter));
+  let letter = [];
+  let maj = 0;
+  let min = 1;
+  let nletter;
+  for (i=0 ; i<longer ; i++)
+   {
+     if (a.slice(i, i+1) == " ")
+     {
+       letter.push(" ");
+     }
+     else
+     {
+        if(maj == '1' && min == 0)
+        {
+          letter.push(a.slice(i, i+1).toUpperCase());
+          maj = 0;
+          min = 1
+        }
+       else if (min == '1'&& maj == 0)
+       {
+          letter.push(a.slice(i, i+1).toLowerCase());
+          maj = 1;
+          min = 0;
+      }
      }
    }
-   console.log(esp);
+    console.log(letter.join(''));
 }
-
 
 maj(c1);
