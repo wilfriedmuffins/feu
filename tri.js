d442d2db0435eb1944a9a21eb6828e98938a9b09
wilfriedmuffins <wilfried.moffen@live.fr>
2020-03-08 18:54:56 +0100

    tri bullet done

--- tri.js
+++ tri.js
@@ -1,34 +1,50 @@
 //const [nd, sc, c1] = process.argv;
-function tri (a){
-  let tab = a;
-  let longer = a.length
+function tri (a) {
+
+  let tab=[];
   let max;
   let min;
-
-
-for(i=0;i<longer;i++){
-  if(tab[i]<tab[i+1]){
-    min=tab[i+1]:
+  let index;
+  let longer = a.length;
+  //console.log(longer);
+  //conversion string argv en int
+  for (x=0; x<longer; x++){
+    tab[x]=parseInt(a[x]);
   }
-}
 
-  //
-  // for ( i=0 ; i<longer ; i++)
-  // {
-  //   //console.log(i);
-  //   for( j=0 ; j<longer ; j++){
-  //     if (tab[j] < tab[j+1]){
-  //       //console.log(j);
+  // for(i=0;i<longer;i++){
+  //   for(j=0;j<longer-1;j++){
+  //     if(tab[j] < tab[j+1]){
   //       max=tab[j+1];
-  //       console.log(max);
+  //       tab[j+1]=tab[j];
+  //       tab[j]=max;
   //     }
-  //     min=tab[i]
-  //     tab[i]=max
-  //     tab[i+1]=min
-  //     //console.log(tab);
   //   }
   // }
-  // //console.log(tab);
+
+  for ( i=0 ; i<longer ; i++)
+  {
+    console.log('ds i'+i);
+    max=tab[i];
+    console.log(max);
+    for( j=0 ; j<longer ; j++)
+    {
+      console.log('ds j'+j);
+      index=j;
+      if ( max < tab[j])
+      {
+        console.log('ds if  de j'+j);
+        max=tab[j];
+        console.log(max);
+      }
+    }
+    console.log(index);
+    console.log(max);
+    min=tab[i];
+    tab[i]=max;
+    tab[i+1]=min
+    console.log(tab);
+  }
 }
 
 tri(process.argv.slice(2));
