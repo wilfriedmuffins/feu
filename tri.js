214471a0c14c464a9ab67f4beac02499bce8a1ec
wilfriedmuffins <wilfried.moffen@live.fr>
2020-03-08 02:27:48 +0100

    making

---
+++ tri.js
@@ -0,0 +1,34 @@
+//const [nd, sc, c1] = process.argv;
+function tri (a){
+  let tab = a;
+  let longer = a.length
+  let max;
+  let min;
+
+
+for(i=0;i<longer;i++){
+  if(tab[i]<tab[i+1]){
+    min=tab[i+1]:
+  }
+}
+
+  //
+  // for ( i=0 ; i<longer ; i++)
+  // {
+  //   //console.log(i);
+  //   for( j=0 ; j<longer ; j++){
+  //     if (tab[j] < tab[j+1]){
+  //       //console.log(j);
+  //       max=tab[j+1];
+  //       console.log(max);
+  //     }
+  //     min=tab[i]
+  //     tab[i]=max
+  //     tab[i+1]=min
+  //     //console.log(tab);
+  //   }
+  // }
+  // //console.log(tab);
+}
+
+tri(process.argv.slice(2));
