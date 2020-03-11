8fecd4f2a19e12c1c137a356c084c324077952b0
wilfriedmuffins <wilfried.moffen@live.fr>
2020-03-12 00:23:24 +0100

    presque doneje dois le deplacer des lettre

--- anag.js
+++ anag.js
@@ -2,11 +2,31 @@ const [nd, sc, c1, file] = process.argv;
 
 var fs = require('fs');
 var string=[]
+var lettre;
+var lanag;
+var word=[];
+var tab=[];
 
 
-console.log(c1);
+
+for(i=0; i<c1.length; i++){
+  word[i]=c1.slice(i, i+1);
+}
+
 fs.readFile(file, 'utf8', function(err, contents){
-  console.log(' le contenu est\n'+contents);
-  string=contents.split();
-  console.log(string);
+  console.log('le contenu est\n'+contents);
+  string=contents.split('\n');
+
+  for(i=0; i<word.length; i++){
+    for(j=0; j<word.length-1 ;j++){
+      lettre=word[j+1];
+      word[j+1]=word[j];
+      word[j]=lettre
+      console.log(word);
+      for(k=0; k<string.length; k++){
+        if( word.join('') == string[k]) tab.push(word.join(''));
+      }
+    }
+  }
+console.log(tab);
 });
