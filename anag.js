7b920258fb02e088b789898aea2f8b095912ed52
wilfriedmuffins <wilfried.moffen@live.fr>
2020-03-23 00:24:20 +0100

    ang sort done

--- anag.js
+++ anag.js
@@ -1,32 +1,74 @@
 const [nd, sc, c1, file] = process.argv;
 
+
 var fs = require('fs');
+
 var string=[]
-var lettre;
-var lanag;
+var lettre
+var lanag=[];
 var word=[];
 var tab=[];
+var data =[]
 
 
+data = fs.readFileSync(file, 'utf8').toString().split('\n')
 
-for(i=0; i<c1.length; i++){
-  word[i]=c1.slice(i, i+1);
+console.log(data)
+
+for(i=0; i<data.length; i++){
+  lettre = data[i].split("").sort().join("")
+   console.log(lettre);
 }
 
-fs.readFile(file, 'utf8', function(err, contents){
-  console.log('le contenu est\n'+contents);
-  string=contents.split('\n');
+  //console.log(data.sort().join());
 
-  for(i=0; i<word.length; i++){
-    for(j=0; j<word.length-1 ;j++){
-      lettre=word[j+1];
-      word[j+1]=word[j];
-      word[j]=lettre
-      console.log(word);
-      for(k=0; k<string.length; k++){
-        if( word.join('') == string[k]) tab.push(word.join(''));
-      }
-    }
-  }
-console.log(tab);
-});
+
+
+
+
+
+
+//
+// for(i=0; i<string.length; i++){
+//   //string[i].sort()
+//   //string[i]=data.split('\n').sort()
+//   //console.log(string[i].sort())
+// }
+
+// string[i]=data.split('\n').sort()
+// console.log(string[i])
+//console.log(string.length);
+
+// for(i=0; i<string.length; i++){
+//   string[i].sort()
+// }
+//console.log(string);
+
+// for(i=0; i<c1.length; i++){
+//   word[i]=c1.slice(i, i+1);
+// }
+
+
+
+
+// fs.readFile(file, 'utf8', function(err, contents){
+//   console.log('le contenu est\n'+contents);
+//   string=contents.split('\n');
+//   console.log(string);
+//
+//
+// });
+//
+// for(i=0; i<fac; i++){
+//   for(j=0; j<c1.length-1 ;j++){
+//     lettre=word[j+1];
+//     word[j+1]=word[j];
+//     word[j]=lettre
+//     console.log(word);
+//     console.log(j);
+//     // for(k=0; k<string.length; k++){
+//     //   if( word.join('') == string[k]) tab.push(word.join(''));
+//     //   console.log(tab);
+//     // }
+//   }
+// }
--- rectangle.js
+++ rectangle.js
@@ -1,13 +1,27 @@
 var fs = require('fs');
+let c1=[];
+let c2=[];
+var a="t";
 
 fs.readFile('c1.txt', 'utf8', function(err, contents){
+  console.log(contents.colunm());
+
+  for (var i=0 ; i<contents.length ; i++){
+    c1[i]=[];
+  }
+
+  // for (var i=0 ; i<c.length ; i++){
+  //   for (var j=0 ; j<contents.length ; j++){
+  //     c1[i][j]=contents.split('');
+  //   }
+  // }
+  console.log(c1);
   console.log('c1:\n'+contents);
+
+  fs.readFile('c2.txt', 'utf8', function(err, contents){
+    console.log(contents.length);
+    console.log('c2:\n'+contents);
+  });
 });
-
-
-fs.readFile('c2.txt', 'utf8', function(err, contents){
-  console.log('c2:\n'+contents);
-});
-
 
 console.log('after calling readFile');
