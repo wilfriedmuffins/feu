59cd06f1e0e595d608807547b2684eaa8d9b1a37
wilfriedmuffins <wilfried.moffen@live.fr>
2020-03-23 09:27:12 +0100

    anagramme done

--- anag.js
+++ anag.js
@@ -3,72 +3,24 @@ const [nd, sc, c1, file] = process.argv;
 
 var fs = require('fs');
 
-var string=[]
-var lettre
-var lanag=[];
-var word=[];
-var tab=[];
+
+var tab=[]
+var anag
+var word = []
 var data =[]
 
+//ouverture du ficher
+data = fs.readFileSync(file, 'utf8').split('\n')
 
-data = fs.readFileSync(file, 'utf8').toString().split('\n')
+//ordre alphabatique du mot
+word=c1.split("").sort().join("")
 
-console.log(data)
-
+//comparaision des mot
 for(i=0; i<data.length; i++){
-  lettre = data[i].split("").sort().join("")
-   console.log(lettre);
+  anag = data[i].split("").sort().join("")
+  if(anag == word)
+  {
+    tab.push(data[i])
+  }
 }
-
-  //console.log(data.sort().join());
-
-
-
-
-
-
-
-//
-// for(i=0; i<string.length; i++){
-//   //string[i].sort()
-//   //string[i]=data.split('\n').sort()
-//   //console.log(string[i].sort())
-// }
-
-// string[i]=data.split('\n').sort()
-// console.log(string[i])
-//console.log(string.length);
-
-// for(i=0; i<string.length; i++){
-//   string[i].sort()
-// }
-//console.log(string);
-
-// for(i=0; i<c1.length; i++){
-//   word[i]=c1.slice(i, i+1);
-// }
-
-
-
-
-// fs.readFile(file, 'utf8', function(err, contents){
-//   console.log('le contenu est\n'+contents);
-//   string=contents.split('\n');
-//   console.log(string);
-//
-//
-// });
-//
-// for(i=0; i<fac; i++){
-//   for(j=0; j<c1.length-1 ;j++){
-//     lettre=word[j+1];
-//     word[j+1]=word[j];
-//     word[j]=lettre
-//     console.log(word);
-//     console.log(j);
-//     // for(k=0; k<string.length; k++){
-//     //   if( word.join('') == string[k]) tab.push(word.join(''));
-//     //   console.log(tab);
-//     // }
-//   }
-// }
+console.log(tab);
