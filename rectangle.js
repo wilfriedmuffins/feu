a419b95045f5e2e44a383c3e79b2e3ea932c221f
wilfriedmuffins <wilfried.moffen@live.fr>
2020-03-23 11:02:50 +0100

    making tab 2 dim

--- rectangle.js
+++ rectangle.js
@@ -1,27 +1,30 @@
+const [nd, sc, file1, file2] = process.argv;
+
 var fs = require('fs');
-let c1=[];
-let c2=[];
-var a="t";
+var tab =[[]]
 
-fs.readFile('c1.txt', 'utf8', function(err, contents){
-  console.log(contents.colunm());
+var data1 = fs.readFileSync(file1, 'utf8').split("\n")
+var data2 = fs.readFileSync(file2, 'utf8')
 
-  for (var i=0 ; i<contents.length ; i++){
-    c1[i]=[];
+console.log(data1);
+console.log(data2);
+console.log(data2.length);
+
+var rec_ref
+var rec_test
+
+
+for (var i=0 ; i<data1.length ; i++){
+  for (var j=0 ; j<length ; j++){
+    c1[i]=[]
   }
+}
 
-  // for (var i=0 ; i<c.length ; i++){
-  //   for (var j=0 ; j<contents.length ; j++){
   //     c1[i][j]=contents.split('');
   //   }
   // }
-  console.log(c1);
-  console.log('c1:\n'+contents);
-
-  fs.readFile('c2.txt', 'utf8', function(err, contents){
-    console.log(contents.length);
-    console.log('c2:\n'+contents);
-  });
-});
-
-console.log('after calling readFile');
+  // console.log('c1:\n'+contents);
+  //
+  //
+  // console.log(contents.length);
+  // console.log('c2:\n'+contents);
