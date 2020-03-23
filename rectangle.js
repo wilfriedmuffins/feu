ad471a659d384a801cdb80cadca6c8f9048da670
wilfriedmuffins <wilfried.moffen@live.fr>
2020-03-23 20:04:12 +0100

    making tab two dim

--- rectangle.js
+++ rectangle.js
@@ -1,30 +1,51 @@
 const [nd, sc, file1, file2] = process.argv;
 
 var fs = require('fs');
-var tab =[[]]
+
+var row
+var colunm
 
 var data1 = fs.readFileSync(file1, 'utf8').split("\n")
-var data2 = fs.readFileSync(file2, 'utf8')
+var data2 = fs.readFileSync(file2, 'utf8').split("\n")
 
-console.log(data1);
-console.log(data2);
-console.log(data2.length);
+// console.log(data1);
+// console.log(data2);
+//number of row and colunm
+colunm = data2[0].length
+row = data2.length-1
+// console.log(colunm);
+//console.log(row);
 
+var tab = new Array(colunm)
+//console.log(tab);
 var rec_ref
 var rec_test
 
 
-for (var i=0 ; i<data1.length ; i++){
-  for (var j=0 ; j<length ; j++){
-    c1[i]=[]
-  }
-}
 
-  //     c1[i][j]=contents.split('');
-  //   }
-  // }
-  // console.log('c1:\n'+contents);
-  //
-  //
-  // console.log(contents.length);
-  // console.log('c2:\n'+contents);
+
+rec_ref = data2.toString().split("")
+//console.log(data2[0].slice(1, 2));
+
+
+// tab[i].push();
+// for (i=0; i<rec_ref.length ; i++){
+//   tab.push("");
+// }
+// console.log(tab);
+
+// if (rec_ref[i] == ','){
+//   console.log(rec_ref[i]);
+// }
+//var r=0
+for (var i=0 ; i<colunm; i++){
+  console.log(i);
+  tab[i]= new Array(row)
+  console.log(tab)
+  for (var j=0 ; j<3 ; j++){
+     //tab[i][j]= data2[i].slice(j, j+1)
+     console.log(data2[i].slice(j, j+1));
+     // console.log(j);
+   }
+  //tab.push([]);
+}
