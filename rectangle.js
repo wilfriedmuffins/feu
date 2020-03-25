3edb86786ce32cbedd43bbe5fcd2a3bec2b2352c
wilfriedmuffins <wilfried.moffen@live.fr>
2020-03-25 08:40:01 +0100

    two tab two dim done

--- rectangle.js
+++ rectangle.js
@@ -2,27 +2,42 @@ const [nd, sc, file1, file2] = process.argv;
 
 var fs = require('fs');
 
-var row
-var colunm
+var row1
+var colunm1
+var row2
+var colunm2
+
 
 var data1 = fs.readFileSync(file1, 'utf8').split("\n")
 var data2 = fs.readFileSync(file2, 'utf8').split("\n")
 
-// console.log(data1);
-// console.log(data2);
-//number of row and colunm
-row = data2.length
-colunm = data2[0].length
+ console.log(data1);
+//console.log(data2);
+//number of row1 and colunm1
+row2 = data2.length
+colunm2 = data2[0].length
+row1 = data1.length
+colunm1 = data1[0].length
+console.log(row1);
+console.log(colunm1);
 
-var tab = new Array(row-1)
-//console.log(tab);
-var rec_ref
-var rec_test
+var tab_ref = new Array(row2-1)
 
-for (i=0 ; i<row-1; i++){
-  tab[i]= new Array(row)
-  for (j=0 ; j<colunm ; j++){
-     tab[i][j]= data2[i].slice(j, j+1)
+// console.log(tab_ref);
+for (i=0 ; i<row2-1; i++){
+  tab_ref[i]= new Array(colunm2)
+  for (j=0 ; j<colunm2 ; j++){
+     tab_ref[i][j]= data2[i].slice(j, j+1)
+   }
+   console.log(tab_ref);
+}
+
+var tab = new Array(row1-1)
+
+for (i=0 ; i<row1-1; i++){
+  tab[i]= new Array(colunm1)
+  for (j=0 ; j<colunm1 ; j++){
+     tab[i][j]= data1[i].slice(j, j+1)
    }
   console.log(tab);
 }
