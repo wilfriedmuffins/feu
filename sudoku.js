7476c289b14eb5d3c955b1e8cfc53db1a37c58cf
wilfriedmuffins <wilfried.moffen@live.fr>
2020-06-10 17:28:53 +0200

    checklinebutnochecking

--- sudoku.js
+++ sudoku.js
@@ -6,37 +6,80 @@ var position=0
 var data1 = fs.readFileSync(file, 'utf8').split("\n")
 
 
+//console.log(data1);
 
-// console.log(data1[0]);
-// for (var i = 0; i < data1.length; i++) {
-//   console.log(data1[i]);
-// }
-//
-console.log(data1);
-//
-// data1.forEach((item) => {
-//   console.log(data1);
-//    sudoku[position]=item.split("")
-//    position +=1
-// });
 data1.forEach((item, i) => {
-  sudoku[position]=item.split("")
+  sudoku[position]=item.split('')
   position++
 });
 
-console.log(sudoku);
 
-for(i=0;i<sudoku.length;i++){
-  for(j=0;j<sudoku[i].length;j++){
-    console.log(sudoku[i][j]);
+// function print_sudoku(sudoku){
+//   for(i=0;i<sudoku.length-1;i++){
+//     //for(j=0;j<sudoku[i].length-1;j++){
+//       console.log(sudoku[i][j]);
+//     //}
+//   }
+// }
+
+function check_line(sudoku,i){
+  // chech si c'est bon
+  for(j=0;j<sudoku[i].length-1;j++){
+    // recherhcé s'il y a deux fois le meme nombre
+    value=sudoku[i][j]
+    same=0
+    for(k=0; k<sudoku[i].length-1;k++){
+      if(value == sudoku[i][k])same++
+      if(same>= 2)return false
+    }
+  }
+  return true// checké la fonciton de return si ce la fait sortir de la boucle ou pas si cond bonne
+}
+
+function solve_sudoku(sudoku){
+  for(i=0;i<sudoku.length-1;i++){
+    for(j=0;j<sudoku[i].length-1;j++){
+      if(sudoku[i][j]=='_'){
+        new_value = 1
+        while (new_value <10){
+          sudoku[i][j]= new_value
+          console.log(sudoku[i][j]+" dans le solve_sudoku");
+          funtion = check_line(sudoku, i)
+          if(funtion){// && check_column(sudoku, j) && check_square(sudoku, i,j)){
+            sudoku = solve_sudoku(sudoku)
+            if (sudoku == false){
+              console.log(sudoku);
+              return false
+            }
+          }
+          new_value++
+         }
+        return false
+      }
+    }
+  }
+  return false
+}
+
+function check_sudoku(sudoku){
+  sudoku = solve_sudoku(sudoku)
+  if (sudoku == false) console.log("erreur")
+  else print_sudoku(sudoku1)
+}
+
+check_sudoku(sudoku)
+
+
+function check_column(sudoku, j){
+  for(i=0;i<sudoku;i++){
+    // if(){}
+    // else{}
   }
 }
 
-// var tab = new Array(data1.length-1)
-//
-// for (i=0 ; i<data1.length-1; i++){
-//   tab[i]= new Array(data1[0].length)
-//   for (j=0 ; j<data1[0].length ; j++){
-//      tab[i][j]= parseInt((data1[i].slice(j, j+1)))
-//    }
-// }
+function check_square(sudoku, i,j){
+  for(i=0;i<sudoku;i++){
+    // if(){}
+    // else{}
+  }
+}
